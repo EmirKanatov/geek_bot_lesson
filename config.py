70566819a35f5d2233b519c83c2238cb08483385
EmirKanatov <laft29@mail.ru>
2022-07-01 03:37:12 +0600

    make fix with token

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 from aiogram import Bot, Dispatcher
-from decouple import config
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
 
 storage = MemoryStorage()
 
-TOKEN = config("TOKEN")
+TOKEN = "5445619231:AAGJzPRVngNIiV2F8hP6s4FoN3qKYNDNDSA"
 bot = Bot(TOKEN)
 dp = Dispatcher(bot=bot, storage=storage)
 ADMIN = [787377130, ]
