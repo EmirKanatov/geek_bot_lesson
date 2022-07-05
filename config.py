80bce99581ba83113fe39af5350f9833b32f3739
EmirKanatov <laft29@mail.ru>
2022-07-05 12:42:28 +0600

    added webhook

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ TOKEN = "5445619231:AAGJzPRVngNIiV2F8hP6s4FoN3qKYNDNDSA"
 bot = Bot(TOKEN)
 dp = Dispatcher(bot=bot, storage=storage)
 ADMIN = [787377130, ]
+URL = "https://python-18-kanatov.herokuapp.com/"
--- main.py
+++ main.py
@@ -5,14 +5,19 @@ from aiogram.utils import executor
 from handlers import notification, fsm_anketa, client, callback, extra, admin
 from fsm import FSMAdmin
 
-from config import dp
+from config import dp, bot, URL
 import logging
 from database.bot_db import sql_create
 
 
 async def on_startup(_):
+    await bot.set_webhook(url=URL)
     asyncio.create_task(notification.scheduler())
     sql_create()
+
+
+async def on_shutdown(dp):
+    await bot.delete_webhook()
 
 
 client.register_handler_client(dp)
@@ -27,4 +32,13 @@ extra.register_handlers_extra(dp)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+    # executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+    executor.start_webhook(
+        dispatcher=dp,
+        webhook_path="",
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host="0.0.0.0",
+        port=5000
+    )
